6a92e5bbd849b0539e61ed0f573d7914b487a8da
Ingrid <ingrid5889@gmail.com>
2024-07-19 14:25:29 -0300

    feat(setup): iniciando o projeto com vue 3, vite, vuex, vue router e tailwind

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1 class="text-3xl text-red">teste</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
