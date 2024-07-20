a17ea14a31042c532c59ce29b821a71108521216
Ingrid <ingrid5889@gmail.com>
2024-07-20 18:18:27 -0300

    chore(dependencies): instalando bibliotecas de toast e o vue apex charts

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue3Toasity from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import VueApexCharts from "vue3-apexcharts"
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(Vue3Toasity)
+app.use(VueApexCharts)
 app.mount('#app')
